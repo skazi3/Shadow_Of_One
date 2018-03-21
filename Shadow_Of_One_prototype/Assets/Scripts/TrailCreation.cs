ef00f52bfcb17375d2164937fef49c00dbe70850
Phill Wasala <pwasal2@uic.edu>
2018-03-21 00:16:47 -0500

    TrailCreation script works now!

--- Shadow_Of_One_prototype/Assets/Scripts/TrailCreation.cs
+++ Shadow_Of_One_prototype/Assets/Scripts/TrailCreation.cs
@@ -5,44 +5,70 @@ using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 public class TrailCreation : MonoBehaviour
-{
-
-
+{ 
     Mesh mesh;
     Vector3[] vertices;
+    List<Vector3> verticeList;
+    List<int> triangleList;
+    public GameObject player;
 
     public Vector3 bLeft;
     public Vector3 tLeft;
     public Vector3 bRight;
     public Vector3 tRight;
-
+    public Vector3 playerPos;
     int[] triangles;
+    private int i = 0;
 
     private void Awake()
     {
+
         mesh = GetComponent<MeshFilter>().mesh;
-    }
-    // Use this for initialization
-    void Start()
-    {
+        
+        //mesh.gameObject.AddComponent<MeshCollider>();
+        playerPos = player.GetComponent<Transform>().position;
+        verticeList = new List<Vector3>();
+        triangleList = new List<int>();
+        bLeft = new Vector3(playerPos.x-1, playerPos.y-.5f, playerPos.z-1);
+        tLeft = new Vector3(playerPos.x - 1, playerPos.y-.5f, playerPos.z);
+        bRight = new Vector3(playerPos.x + 1, playerPos.y-.5f, playerPos.z - 1);
+        tRight = new Vector3(playerPos.x + 1, playerPos.y-.5f, playerPos.z);
+
         MakeMeshData();
         CreateMesh();
-
     }
+
     private void Update()
     {
+        playerPos = player.GetComponent<Transform>().position;
+        UpdateVertices();
+        CreateMesh();
+        MeshCollider mesh_collider = GetComponent<MeshCollider>();
+        mesh_collider.sharedMesh = mesh;
+
 
     }
+    
 
 
     void MakeMeshData()
     {
         //create array of vertices
-        vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 0) };
-
+        //vertices = new Vector3[] { bLeft, tLeft, bRight, tRight };
+        verticeList.Add(bLeft);
+        verticeList.Add(tLeft);
+        verticeList.Add(bRight);
+        verticeList.Add(tRight);
+        vertices = verticeList.ToArray();
         //creates array of integers
-
-        triangles = new int[] { 0, 1, 2 };
+        triangleList.Add(i);
+        triangleList.Add(i + 1);
+        triangleList.Add(i + 2);
+        triangleList.Add(i + 2);
+        triangleList.Add(i + 1);
+        triangleList.Add(i + 3);
+        triangles = triangleList.ToArray();
+        //triangles = new int[] { i, i+1, i+2, i+2, i+1, i+3 };
     }
 
     void CreateMesh()
@@ -50,7 +76,31 @@ public class TrailCreation : MonoBehaviour
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+    }
 
+    void UpdateVertices()
+    {
+        bLeft = tLeft;
+        bRight = tRight;
+        tLeft = new Vector3(playerPos.x - 1, playerPos.y-.5f, playerPos.z);
+        tRight = new Vector3(playerPos.x + 1, playerPos.y - .5f, playerPos.z);
+        Vector3 newBLeft = new Vector3(bLeft.x, bLeft.y, bLeft.z);
+        Vector3 newBRight = new Vector3(bRight.x, bRight.y, bRight.z);
+        Vector3 newTRight = new Vector3(tRight.x, tRight.y, tRight.z);
+        Vector3 newTLeft = new Vector3(tLeft.x, tLeft.y, tLeft.z);
+        verticeList.Add(newBLeft);
+        verticeList.Add(newTLeft);
+        verticeList.Add(newBRight);
+        verticeList.Add(newTRight);
+        vertices = verticeList.ToArray();
+        i += 4;
+        triangleList.Add(i);
+        triangleList.Add(i+1);
+        triangleList.Add(i+2);
+        triangleList.Add(i+2);
+        triangleList.Add(i+1);
+        triangleList.Add(i+3);
+        triangles = triangleList.ToArray();
     }
 
 
