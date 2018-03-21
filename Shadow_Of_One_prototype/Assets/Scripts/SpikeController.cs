8f14062ea87cf7600efed0f1344f574d290a9a49
sarah kazi <skazi3@uic.edu>
2018-03-21 14:59:47 -0500

    final

--- Shadow_Of_One_prototype/Assets/Scripts/SpikeController.cs
+++ Shadow_Of_One_prototype/Assets/Scripts/SpikeController.cs
@@ -11,26 +11,57 @@ public class SpikeController : MonoBehaviour {
 		isPlayerNear = false;
 		moveUp = false;
 	}
-	void moveSpikesUp(){
-		if (!isPlayerNear) {
-			transform.Translate (new Vector3 (0, 7, 0));
-		}
-		isPlayerNear = true;
-	}		
 	// Update is called once per frame
 	void Update () {
 		if (player.transform.position.z >= 800)
-			moveSpikesUp ();
-		if (transform.position.y <= -10.4)
-			moveUp = true;
-		else if(transform.position.y >= 6)
-			moveUp = false;
-			
-		if (moveUp) {
-			transform.Translate (new Vector3 (0, 1, 0));
-		} else if (!moveUp) {
-			transform.Translate (new Vector3 (0, -1, 0));
+		{
+			isPlayerNear = true;
 		}
+		if (isPlayerNear == true)
+		{
+			startCycle ();
+		}
+	}
+	void startCycle(){
+		if (transform.position.y >= 5.5) {
+			moveUp = false;
+		} else if (transform.position.y <= -10.4)
+			moveUp = true;
+		if(moveUp)
+			transform.Translate(0, 1, 0);
+		else
+			transform.Translate(0, -1, 0);
+	}
+	void MoveUp()
+	{
+		
+		if (transform.position.y >= 5.5)
+		{
 
+			MoveDown();
+		}
+		else
+			transform.Translate(0, 1, 0);
+	}
+
+	void MoveDown()
+	{
+		
+		if (transform.position.y <= -10.4)
+		{
+			Debug.Log ("In move down if statement,");
+			MoveUp();
+		}
+		else 
+			transform.Translate(0, -1, 0);
+	}
+
+	void moveSpikesUp()
+	{
+		if (!isPlayerNear)
+		{
+			transform.Translate(new Vector3(0, 7, 0));
+		}
+		isPlayerNear = true;
 	}
 }
