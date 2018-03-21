90d75df6ec984a55b36f166452ffa747712bee6f
sarah kazi <skazi3@uic.edu>
2018-03-20 23:12:59 -0500

    adding spike script

---
+++ Shadow_Of_One_prototype/Assets/SpikeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeController : MonoBehaviour {
+	public GameObject player;
+	private bool playerIsNear;
+	// Use this for initialization
+	void Start () {
+		playerIsNear = false;
+	}
+	void moveSpikesUp(){
+		if (!playerIsNear) {
+			transform.Translate(new Vector3(0, 10, 0));
+		}
+		playerIsNear = true;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (player.transform.position.z >= 800) {
+			moveSpikesUp ();
+		}
+	}
+}
