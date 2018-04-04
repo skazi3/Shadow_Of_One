c50bb822f2c2c91db38ae886d82b964e3f23c926
Sarah Kazi <skazi3@uic.edu>
2018-04-04 00:14:08 -0500

    Obstacles change color

---
+++ Shadow_Of_One_prototype/Assets/Scripts/ColorScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorScript : MonoBehaviour {
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnCollisionEnter(Collision collision){
+		if (collision.collider.tag == "Player2") {
+			Debug.Log ("Entered");
+			transform.GetComponent<Renderer> ().material.color = Color.red;
+		}
+		else
+			transform.GetComponent<Renderer>().material.color = Color.white;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
