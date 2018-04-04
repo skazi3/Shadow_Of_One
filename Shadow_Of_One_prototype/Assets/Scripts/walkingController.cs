c0527eaab9293eecc443cbb2b52e174b209cc974
Sarah Kazi <skazi3@uic.edu>
2018-04-04 12:40:43 -0500

    Working on walking animation

---
+++ Shadow_Of_One_prototype/Assets/Scripts/walkingController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkingController : MonoBehaviour {
+	private Animator animator;
+	// Use this for initialization
+	void Awake(){
+		animator = GetComponent<Animator> ();
+
+	}
+	void Update(){
+		if (Input.GetKey (KeyCode.W)) {
+			animator.SetBool ("keyPressed", true);
+		} else
+			animator.SetBool ("keyPressed", false);
+	}
+}
