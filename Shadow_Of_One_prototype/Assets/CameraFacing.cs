e66809048bcef27667adf3f84615eb4203b23bda
sarah kazi <skazi3@uic.edu>
2018-04-11 12:22:12 -0500

    adding billboard

---
+++ Shadow_Of_One_prototype/Assets/CameraFacing.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFacing : MonoBehaviour
+{
+	public Camera cam1, cam2;
+
+	void Update()
+	{
+		transform.LookAt(transform.position + cam1.transform.rotation * Vector3.forward,
+			cam1.transform.rotation * Vector3.up);
+		
+		transform.LookAt(transform.position + cam2.transform.rotation * Vector3.forward,
+			cam2.transform.rotation * Vector3.up);
+	}
+}
---
+++ Shadow_Of_One_prototype/Assets/Scripts/Player1Walking.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Walking : MonoBehaviour {
+	private Animator animator2;
+	// Use this for initialization
+	void Awake(){
+		animator2 = GetComponent<Animator> ();
+	}
+	void Update(){
+		
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate(5 * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 5 * Input.GetAxis("Vertical") * Time.deltaTime);
+			animator2.SetBool ("upArrowPressed", true);
+		} else
+			animator2.SetBool ("upArrowPressed", false);
+	}
+}
