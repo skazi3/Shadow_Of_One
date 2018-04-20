338373178607cdc329b8ae1f106025e709268208
sarah kazi <skazi3@uic.edu>
2018-04-20 13:33:01 -0500

    ADDING Scener

--- Shadow_Of_One/Assets/KeyCollection.cs
+++ Shadow_Of_One/Assets/KeyCollection.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyCollection : MonoBehaviour {
+	public GameObject door;
 
 	/**/
 	void OnCollisionEnter(Collision collision)
 	{
-		Destroy (this);
+		Destroy (this.gameObject);
+		Destroy (door);
+
 		
 	}
 	
--- Shadow_Of_One/Assets/Scripts/Player2Walking.cs
+++ Shadow_Of_One/Assets/Scripts/Player2Walking.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 
 public class Player2Walking : MonoBehaviour {
 	private Animator animator;
+	private bool canJump;
 	public float speed = 10.0F;
 	public float rotationSpeed = 100.0F;
 	// Use this for initialization
 	void Awake(){
 		animator = GetComponent<Animator> ();
-
+		canJump = false;
 	}
 	void Update(){
 		float translation = Input.GetAxis("Vertical") * speed;
@@ -20,6 +21,13 @@ public class Player2Walking : MonoBehaviour {
 		rotation *= Time.deltaTime;
 		transform.Translate(0, 0, translation);
 		transform.Rotate(0, rotation, 0);
+		//check if player reached first checkpoint
+		if (transform.position.z > 57 && transform.position.z < 63) {
+			if (transform.position.x > 38 && transform.position.x > 43) {
+				canJump = true;
+			}
+		}
+
 		if (Input.GetKey (KeyCode.W)) {
 	
 			animator.SetBool ("keyPressed", true);
@@ -27,12 +35,15 @@ public class Player2Walking : MonoBehaviour {
 			animator.SetBool ("keyPressed", false);
 		}
 		if (Input.GetKey (KeyCode.Space)) {
-			Debug.Log ("truueeee");
-			animator.SetBool ("jumpPressed", true);
+			if (canJump) {
+				Debug.Log ("TRUEEE:");
+				animator.SetBool ("jumpPressed", true);
+			}
 		} else if(Input.GetKeyUp(KeyCode.Space))
 		{
-			animator.SetBool ("jumpPressed", false);
-			Debug.Log ("falseee");
+			if(canJump)
+				animator.SetBool ("jumpPressed", false);
+		
 		}
 	}
 }
