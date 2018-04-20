cc622678c8013ee121ef5b90eb94eae2964b9252
sarah kazi <skazi3@uic.edu>
2018-04-20 13:09:40 -0500

    animations look good

---
+++ Shadow_Of_One/Assets/KeyCollection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCollection : MonoBehaviour {
+
+	/**/
+	void OnCollisionEnter(Collision collision)
+	{
+		Destroy (this);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
